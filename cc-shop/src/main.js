8bd59be635ceb07225479279acb31c4826f7a6f0
ylm <yingleiming@outlook.com>
2020-02-05 11:57:51 +0800

    全局配置fastclick favicon.ico 商城名称

--- cc-shop/src/main.js
+++ cc-shop/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import FastClick from "fastclick"
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body);
+  }, false);
+}
+
 
 Vue.config.productionTip = false
 
