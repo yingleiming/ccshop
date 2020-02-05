f3c5c9809156c4c9d1e0b4d71b9238affab5b451
ylm <yingleiming@outlook.com>
2020-02-05 12:58:35 +0800

    创建主页

---
+++ cc-shop/src/views/category/Category.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="category"></div>
+</template>
+
+<script>
+    export default {
+        name: "Category"
+    }
+</script>
+
+<style lang="less" scoped>
+    #category{
+        width: 100%;
+        height: 100%;
+        background-color: green;
+    }
+</style>
+
---
+++ cc-shop/src/views/mine/Mine.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="mine"></div>
+</template>
+
+<script>
+    export default {
+        name: "Mine"
+    }
+</script>
+
+<style lang="less" scoped>
+    #mine{
+        width: 100%;
+        height: 100%;
+        background-color: purple;
+    }
+</style>
