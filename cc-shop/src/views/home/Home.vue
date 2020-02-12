7035a562a6cbd49f426bd9b1a88c34ea610606dd
ylm <yingleiming@outlook.com>
2020-02-12 17:29:31 +0800

    nav

--- cc-shop/src/views/home/Home.vue
+++ cc-shop/src/views/home/Home.vue
@@ -3,6 +3,7 @@
         <div v-if="!showLoading">
             <Header />
             <Sowing :sowing_list="sowing_list"/>
+            <Nav :nav_list="nav_list"/>
         </div>
         <van-loading
             v-else
@@ -21,15 +22,20 @@
     //2.引入组件
     import Header from "./components/header/Header"
     import Sowing from "./components/sowing/Sowing"
+    import Nav from "./components/nav/Nav"
     export default {
         name: "Home",
         components:{//注册组件
             Header,
-            Sowing
+            Sowing,
+            Nav
         },
         data(){
             return {
+                //轮播图数据
                 sowing_list:[],
+                //导航数据
+                nav_list:[],
                 showLoading:true
             }
         },
@@ -37,6 +43,7 @@
             //2.请求网络数据
             getHomeData().then((response)=>{
                 this.sowing_list = response.data.list[0].icon_list;
+                this.nav_list = response.data.list[2].icon_list;
                 this.showLoading = false;
             })
         }
