bf8ddf0f9a3df22e0617d9bf344c4971a2ae4202
ylm <yingleiming@outlook.com>
2020-02-17 12:09:28 +0800

    ~~~~

---
+++ cc-shop/src/views/home/components/flashSale/flashSale.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "flashSale"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ cc-shop/src/views/home/components/flashSale/flashSaleItem.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "flashSaleItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
